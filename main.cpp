f9466f5b3a602481ea0791757d4d3d9bcabeb8fb
Alex <empty@gmail.com>
2024-10-16 21:18:55 +0300

    developed user interface

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
-int main() {
+int main() 
+{
+
+	std::cout << "\tCONSOLE GAME BUNDLE\t" << std::endl;
+	// load bundle discription from txt file
+
+	std::cout << " To quit game write \'exit\' in console" << std::endl;
+
+	while (true)
+	{
+		std::cout << " In which game do you want to play: \n   - Rock-Paper-Scissors (r)\n   - Casino Guessing Number (c)\n   - Hangman Game (h)" << std::endl;
+		std::cout << " enter one symbol > ";
+		std::string answer;
+		std::cin >> answer;
+
+		if (answer == "exit")
+		{
+			std::cout << "\n\tGood Bye!\n" << std::endl;
+			break;
+		}
+		else if (answer == "r") std::cout << " Chosen Rock-Paper-Scissors" << std::endl;
+		else if (answer == "c")std::cout << " Chosen Casino Guessing Number" << std::endl;
+		else if (answer == "h")std::cout << " Chosen Hangman Game" << std::endl;
+		else 
+		{
+			std::cout << " There is no such command, watch the \"GameCommand.md\" and try again." << std::endl;
+		}
+
+
+		std::cout << std::endl;
+	}
+
+	
 
 
 	return 0;
