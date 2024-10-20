fdec0d36b2279c4124708612f9bd651fdcf9cf68
Alex <empty@gmail.com>
2024-10-20 04:05:04 +0300

    Developed game logic

--- CasinoNumberGuessing.cpp
+++ CasinoNumberGuessing.cpp
@@ -19,12 +19,13 @@ void CasinoNumberGuessing()
 
 	while (true) // main loop
 	{
-		// setting the level difficulty
+		// setting the level difficulty.
 		int difficulty = 0;
-		std::cout << " Choose the game difficulty: Easy(1), Mediun(2), Hard(3)" << std::endl;
+		std::cout << " \nChoose the game difficulty: Easy(1), Mediun(2), Hard(3)" << std::endl;
 		std::cout << " Your difficulty > ";
 		std::cin >> uInput;
 
+		// input validation.
 		if (uInput == "exit")return;
 		else if (uInput.find_first_not_of("0123456789") != std::string::npos)
 		{
@@ -39,12 +40,14 @@ void CasinoNumberGuessing()
 			continue;
 		}
 
+
+		// game set up.
 		int trials = 3;
 		int min = rand() % 1000;
 		int max = min + difficulty * 4 - 1;
 		number = min + rand() % (max - min + 1);// min + rand()%(max - min +1)
 
-		// guessing the number
+		// guessing the number.
 		for (; trials > 0 && coins; trials--)
 		{
 			std::cout << std::endl;
@@ -54,6 +57,7 @@ void CasinoNumberGuessing()
 			std::cout << " Your number > ";
 			std::cin >> uInput;
 
+			// input validation.
 			if (uInput == "exit")return;
 			else if (uInput.find_first_not_of("0123456789") != std::string::npos)
 			{
@@ -61,10 +65,40 @@ void CasinoNumberGuessing()
 				trials++;
 				continue;
 			}
+
+			uNumber = std::stoi(uInput);
+			if (uNumber == number)
+			{
+				coins += 100;
+				std::cout << " You have guessed the number. You won 100 coins." << std::endl;;
+				break;
+			}
+			else if (uNumber < number)
+			{
+				coins -= 100;
+				std::cout << " You didn't guess the number, your number is lower. You lost 100 coins." << std::endl;
+			}
+			else
+			{
+				coins -= 100;
+				std::cout << " You didn't guess the number, your number is higher. You lost 100 coins." << std::endl;
+			}
+
+			if (coins == 1000)
+			{
+				std::cout << " You win the casino." << std::endl;
+				coins = 500;
+				break;
+			}
+			else if (coins == 0)
+			{
+				std::cout << " You lost the casino." << std::endl;
+				coins = 500;
+				break;
+			}
+
+			uInput.clear();
 		}
-
-
-
 			
 	}
 }
