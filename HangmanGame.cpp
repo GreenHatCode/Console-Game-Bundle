867a69db7cdaae5c54384d5a293e6babece77483
Alex <empty@gmail.com>
2024-10-23 12:14:26 +0300

    Added resource files for the game. Also implemented outputing the description of the game

---
+++ HangmanGame.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Text.h"
+
+void HangmanGame()
+{
+	// executes game Casino-Number-Guessing 
+	std::cout << "\n\tCASINO-NUMBER-GUESSING" << std::endl;
+
+	// load game description and rules from rock-paper-scissors.txt
+	PrintTextInFrame("HangmanGameAbout.txt", 45);
+
+	// game set up
+
+
+
+}
